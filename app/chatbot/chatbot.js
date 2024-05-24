b338108e6ade82684aab1def08cc30abc56ecd90
Robin Cui <honglei9423@gmail.com>
2024-05-23 22:55:03 -0600

    Fixed the "Synchronous scripts should not be used" problem.

--- app/chatbot/chatbot.js
+++ app/chatbot/chatbot.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Script from 'next/script'
 
 function Chatbot(){
     return(
         <div>
-            <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
-            <script src="https://mediafiles.botpress.cloud/eacbf454-69c0-4f33-8a92-f53059a128af/webchat/config.js" defer></script>
+            <Script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></Script>
+            <Script src="https://mediafiles.botpress.cloud/eacbf454-69c0-4f33-8a92-f53059a128af/webchat/config.js" defer></Script>
         </div>
     )
 }
