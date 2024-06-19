94e5866ad101de73f206169cea54bf565d6acbc9
Robin Cui <honglei9423@gmail.com>
2024-06-19 15:43:26 -0600

    Finished the header part of the home page except for the profile image.

--- app/components/header.js
+++ app/components/header.js
@@ -9,12 +9,113 @@ import { useUserAuth } from '../_utils/auth-context';
 
 import logo from '../assets/images/logo.png';
 
+// Menu component
+function Menu() {
+
+  const router = useRouter();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  /*
+  Function to handle category selection.
+  This function is called whenever the user selects a category from the dropdown menu
+  and redirects the user to the 'showproducts' page with the selected category as a query parameter.
+  */
+  const handleCategorySelect = (category) => {
+    router.push(`/showproducts?category=${encodeURIComponent(category)}`);
+  };
+
+  return (
+    <nav className="flex space-x-12 mx-auto items-center">
+
+      {/* Categories */}
+      <div
+        className="relative pb-1"
+        onMouseEnter={() => setIsDropdownOpen(true)}
+        onMouseLeave={() => setIsDropdownOpen(false)}
+      >
+        <a className="text-lg font-bold hover:text-[#FF8811] cursor-pointer">
+          {/* TODO use the react icon, not the ↓ */}
+          Categories ↓
+        </a>
+        {isDropdownOpen && (
+          <div className="absolute z-10 mt-1 bg-white rounded-md shadow-lg w-48">
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              onClick={() => handleCategorySelect("Automotive")}
+            >
+              Automotive
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              onClick={() => handleCategorySelect("Beauty")}
+            >
+              Beauty
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              onClick={() => handleCategorySelect("Electronics")}
+            >
+              Electronics
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              onClick={() => handleCategorySelect("Fashion")}
+            >
+              Fashion
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              onClick={() => handleCategorySelect("Food and Groceries")}
+            >
+              Food & Groceries
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              onClick={() => handleCategorySelect("Home & Garden")}
+            >
+              Home & Garden
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              onClick={() => handleCategorySelect("Sports")}
+            >
+              Sports
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              onClick={() => handleCategorySelect("Others")}
+            >
+              Others
+            </a>
+          </div>
+        )}
+      </div>
+
+      {/* Sell Products */}
+      <Link href="/sell" className="text-lg font-bold hover:text-[#FF8811]">
+        Sell Products
+      </Link>
+
+      {/* About */}
+      <Link href="/about" className="text-lg font-bold hover:text-[#FF8811]" >
+        About
+      </Link>
+    </nav>
+  );
+}
+
 function Header() {
-  
+
   const router = useRouter();
   const { user, firebaseSignOut } = useUserAuth();
-
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handleSignIn = () => {
     router.push('/login');
@@ -29,120 +130,30 @@ function Header() {
     }
   };
 
-  /*
-  Function to handle category selection.
-  This function is called whenever the user selects a category from the dropdown menu
-  and redirects the user to the 'showproducts' page with the selected category as a query parameter.
-  */
-  const handleCategorySelect = (category) => {
-    router.push(`/showproducts?category=${encodeURIComponent(category)}`);
-  };
-
   return (
     <header className="bg-[#FFF8F0]">
-      <div className="max-w-5xl h-16 flex justify-center items-center mx-auto">
+      <div className="max-w-5xl h-16 flex justify-between items-center mx-auto">
+
         {/* Logo */}
-        <Link href="/">
+        <Link href="/" className="ml-9">
           <Image src={logo} alt="Agora BNS Logo" width={48} height={43} />
         </Link>
 
         {/* Menu */}
-        {true && (<nav className="flex space-x-10 mx-auto items-center">
-
-          {/* Categories */}
-          <div
-            className="relative pb-1"
-            onMouseEnter={() => setIsDropdownOpen(true)}
-            onMouseLeave={() => setIsDropdownOpen(false)}
-          >
-            <a className="text-lg font-bold hover:text-[#FF8811] cursor-pointer">
-              {/* TODO use the react icon, not the ↓ */}
-              Categories ↓
-            </a>
-            {isDropdownOpen && (
-              <div className="absolute z-10 mt-1 bg-white rounded-md shadow-lg w-48">
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  onClick={() => handleCategorySelect("Automotive")}
-                >
-                  Automotive
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  onClick={() => handleCategorySelect("Beauty")}
-                >
-                  Beauty
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  onClick={() => handleCategorySelect("Electronics")}
-                >
-                  Electronics
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  onClick={() => handleCategorySelect("Fashion")}
-                >
-                  Fashion
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  onClick={() => handleCategorySelect("Food and Groceries")}
-                >
-                  Food & Groceries
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  onClick={() => handleCategorySelect("Home & Garden")}
-                >
-                  Home & Garden
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  onClick={() => handleCategorySelect("Sports")}
-                >
-                  Sports
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  onClick={() => handleCategorySelect("Others")}
-                >
-                  Others
-                </a>
-              </div>
-            )}
+        {true && 
+          <div className="flex flex-1">
+            <Menu />
           </div>
-
-          {/* Sell Products */}
-          <Link href="/sell" className="text-lg font-bold hover:text-[#FF8811]">
-            Sell Products
-          </Link>
-
-          {/* About */}
-          <Link href="/about" className="text-lg font-bold hover:text-[#FF8811]" >
-            About
-          </Link>
-        </nav>)}
+        }
 
         {/* Sign In/Logout Button */}
-        <div className="flex items-center justify-end flex-1 lg:w-0">
+        <div className="flex items-center mr-9">
           {user ? (
             <>
-              <span className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-                {/* TODO get name from the database if there is no displayName, - login with Email. */}
-                {user.displayName ? `Welcome, ${user.displayName}` : 'Welcome!'}
-              </span>
+              {/* TODO profile image */}
               <button
                 onClick={handleSignOut}
-                className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gradient-to-r from-teal-500 to-cyan-600 hover:bg-gradient-to-br"
+                className="text-lg font-bold text-[#FF8811] hover:text-[#392F5A]"
               >
                 Logout
               </button>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 "use client";
 
 import Head from 'next/head';
+
+// Components for common header, footer.
 import Header from './components/header';
 import Footer from './components/footer';
 
-// Components for this landing page.
+// Components for this home page.
 import Intro from './components/home/intro';
 import FeaturedProducts from './components/home/featured-products';
 import FeaturedCategories from './components/home/featured-categories';
@@ -20,34 +22,13 @@ const Home = () => {
       </Head>
 
       <Header />
+
       <Intro />
       <FeaturedProducts />
       <FeaturedCategories />
       <FAQ />
+
       <Footer />
-
-
-      {/* <main>
-        <div className="bg-gradient-to-r from-purple-800 to-indigo-700 flex flex-col justify-center items-center text-center py-16 lg:py-32">
-          <div>
-            <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
-              Discover Amazing Products
-            </h1>
-            <p className="mt-4 max-w-lg mx-auto text-xl text-indigo-200">
-              Sign In Required to view products in the Agora-BNS Marketplace.
-            </p>
-            <div className="mt-10">
-              <button
-                onClick={handleSignIn}
-                className="inline-block bg-indigo-600 text-white px-5 py-3 rounded-lg shadow-lg text-base font-medium hover:bg-indigo-700"
-              >
-                Sign In to View Products
-              </button>
-            </div>
-          </div>
-        </div>
-      </main> */}
-      
     </>
   );
 }
