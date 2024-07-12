0cd7beb0143a236157158f6eb7f1f23fdcb907f1
partharora729 <130489526+partharora729@users.noreply.github.com>
2024-07-12 15:10:00 -0600

    commit- 2

--- app/_components/Map.js
+++ app/_components/Map.js
@@ -1,13 +1,81 @@
+// import React, { useState, useEffect } from "react";
+// import { MapContainer, TileLayer, Marker } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
+// import L from "leaflet";
+// import { doc, getDoc } from "firebase/firestore";
+// import { db } from "../_utils/firebase"; 
+
+// const Map = () => {
+//   const [userPosition, setUserPosition] = useState(null); 
+//   const [locationError, setLocationError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchUserLocation = async () => {
+//       try {
+//         const docRef = doc(db, "locations", "userLocation"); 
+//         const docSnap = await getDoc(docRef);
+
+//         if (docSnap.exists()) {
+//           const data = docSnap.data();
+//           const latitude = data.latitude; 
+//           const longitude = data.longitude;
+//           setUserPosition([latitude, longitude]);
+//           console.log(`User's location from Firebase: Latitude: ${latitude}, Longitude: ${longitude}`);
+          
+//         } else {
+//           console.log("No such document!");
+//         }
+//       } catch (error) {
+//         setLocationError(error.message);
+//         console.error("Error fetching user location from Firebase:", error);
+//       }
+//     };
+
+//     fetchUserLocation();
+//   }, [userPosition]);
+//   console.log(userPosition)
+
+//   const customIcon = new L.Icon({
+//     iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
+//     iconSize: [38, 38], // size of the icon
+//   });
+
+//   return (
+//     userPosition && (
+//       <div>
+//         {locationError && <p>Error fetching location: {locationError}</p>}
+//         <MapContainer
+//           center={userPosition}
+//           zoom={15}
+//           style={{ height: "400px", width: "100%" }}
+//         >
+//           <TileLayer
+//             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//           />
+//           <Marker position={userPosition} icon={customIcon} />
+//         </MapContainer>
+//       </div>
+//     )
+//   );
+// };
+
+// export default Map;
+
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import { doc, getDoc } from "firebase/firestore";
-import { db } from "../_utils/firebase"; 
+import { db } from "../_utils/firebase";
+
+const containerStyle = {
+  width: '100%',
+  height: '300px', // Increased height for better visibility and alignment
+};
 
 const Map = () => {
   const [userPosition, setUserPosition] = useState(null); 
   const [locationError, setLocationError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUserLocation = async () => {
@@ -19,44 +87,43 @@ const Map = () => {
           const data = docSnap.data();
           const latitude = data.latitude; 
           const longitude = data.longitude;
-          setUserPosition([latitude, longitude]);
+          setUserPosition({ lat: latitude, lng: longitude });
           console.log(`User's location from Firebase: Latitude: ${latitude}, Longitude: ${longitude}`);
-          
         } else {
           console.log("No such document!");
         }
       } catch (error) {
         setLocationError(error.message);
         console.error("Error fetching user location from Firebase:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchUserLocation();
-  }, [userPosition]);
-  console.log(userPosition)
-
-  const customIcon = new L.Icon({
-    iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
-    iconSize: [38, 38], // size of the icon
-  });
+  }, []);
 
   return (
-    userPosition && (
-      <div>
-        {locationError && <p>Error fetching location: {locationError}</p>}
-        <MapContainer
-          center={userPosition}
-          zoom={15}
-          style={{ height: "400px", width: "100%" }}
+    <div className="">
+      {loading && <p className="text-gray-700 text-center p-4">Loading map...</p>}
+      {locationError && <p className="text-red-500 text-center p-4">Error fetching location: {locationError}</p>}
+      {userPosition && (
+        <LoadScript 
+          googleMapsApiKey="AIzaSyBeA_iNOUh8bLpdvuJ6g4OYhqofLCQvhUw"
+          onError={(error) => console.error("Error loading Google Maps script:", error)}
+          onLoad={() => console.log("Google Maps script loaded successfully")}
         >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <Marker position={userPosition} icon={customIcon} />
-        </MapContainer>
-      </div>
-    )
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={userPosition}
+            zoom={15}
+            className="rounded-lg shadow-lg h-full"
+          >
+            <Marker position={userPosition} />
+          </GoogleMap>
+        </LoadScript>
+      )}
+    </div>
   );
 };
 
--- app/details/page.js
+++ app/details/page.js
@@ -7,7 +7,7 @@ import Image from 'next/image';
 
 import Header from '../_components/header';
 import Footer from '../_components/footer';
-import Map from 'app/_components/Map';
+import Map from '../_components/Map';  // Assuming this is the correct path to your Map component
 import { db } from '../_utils/firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import { addComment, getComments } from '../_services/comment-service';
@@ -67,7 +67,6 @@ function ProductDetails() {
       }
     }
   };
-  
 
   if (!product) return <div className="text-center mt-20">Loading...</div>;
 
@@ -81,95 +80,97 @@ function ProductDetails() {
 
       <main className="bg-gray-100 pt-24 pb-12 flex-grow">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <div className="flex flex-col md:flex-row">
-              <div className="flex-1 flex justify-center items-center">
-                <Image
-                  src={mainImage}
-                  alt={product.name}
-                  width={600}
-                  height={400}
-                  objectFit="contain"
-                  className="rounded-md shadow-md"
-                />
-              </div>
-              <div className="flex-1 p-6">
-                <h1 className="text-4xl font-bold text-gray-800">{product.name}</h1>
-                <p className="text-2xl text-gray-600 my-4">{product.price ? `$${product.price}` : 'Free'}</p>
-                <p className="text-lg text-gray-700 mb-4">{product.description}</p>
-                <p className="text-sm text-gray-500 mb-1"><strong>Category:</strong> {product.category}</p>
-                <p className="text-sm text-gray-500 mb-4"><strong>Condition:</strong> {product.condition}</p>
-                <div className="flex flex-wrap space-x-2 mt-4">
-                  {product.imageUrls.map((url, index) => (
-                    <div key={index} className="w-24 h-24 relative cursor-pointer" onClick={() => handleImageClick(url)}>
-                      <Image
-                        src={url}
-                        alt={`${product.name} image ${index + 1}`}
-                        layout="fill"
-                        objectFit="contain"
-                        className="rounded-md border border-gray-300"
-                      />
-                    </div>
-                  ))}
-                </div>
-                <div className="mt-6">
-                  <button 
-                    onClick={() => router.back()} 
-                    className="text-blue-600 hover:text-blue-800 font-semibold inline-flex items-center"
-                  >
-                    ← Back to Products
-                  </button>
-                  <div>
-                    <Map/>
-                  </div>
-                </div>
-              </div>
+          <div className="bg-white p-6 rounded-lg shadow-lg md:flex">
+            <div className="flex-1 flex justify-center items-center">
+              <Image
+                src={mainImage}
+                alt={product.name}
+                width={600}
+                height={400}
+                objectFit="contain"
+                className="rounded-md shadow-md"
+              />
             </div>
-            <div className="mt-8">
-              <h2 className="text-2xl font-bold text-gray-800">Comments and Ratings</h2>
-              <div className="mt-4">
-              {comments.map((comment, index) => (
-                  <div key={index} className="mb-4 p-4 border rounded-lg shadow-sm">
-                    <p className="text-sm text-gray-600">{comment.date}</p>
-                    <p className="text-lg text-gray-800">{comment.text}</p>
-                    <p className="text-sm text-gray-600">Rating: {comment.rating}/5</p>
+            <div className="flex-1 p-6">
+              <h1 className="text-4xl font-bold text-gray-800">{product.name}</h1>
+              <p className="text-2xl text-gray-600 my-4">{product.price ? `$${product.price}` : 'Free'}</p>
+              <p className="text-lg text-gray-700 mb-4">{product.description}</p>
+              <p className="text-sm text-gray-500 mb-1"><strong>Category:</strong> {product.category}</p>
+              <p className="text-sm text-gray-500 mb-4"><strong>Condition:</strong> {product.condition}</p>
+              <div className="flex flex-wrap space-x-2 mt-4">
+                {product.imageUrls.map((url, index) => (
+                  <div key={index} className="w-24 h-24 relative cursor-pointer" onClick={() => handleImageClick(url)}>
+                    <Image
+                      src={url}
+                      alt={`${product.name} image ${index + 1}`}
+                      layout="fill"
+                      objectFit="contain"
+                      className="rounded-md border border-gray-300"
+                    />
                   </div>
                 ))}
               </div>
-              <div className="mt-4">
-
-                <textarea
-                  className="w-full p-2 border rounded-lg text-black"
-                  rows="3"
-                  placeholder="Add your comment..."
-                  value={newComment}
-                  onChange={(e) => setNewComment(e.target.value)}
-                />
-                <div className="mt-2">
-                  <label className="text-gray-600">Rating: </label>
-                  <select
-                    className="ml-2 p-1 border rounded-lg text-black"
-                    value={rating}
-                    onChange={(e) => setRating(Number(e.target.value))}
-                  >
-                    <option value="0">Select Rating</option>
-                    {[1, 2, 3, 4, 5].map(r => (
-                      <option key={r} value={r}>{r}</option>
-                    ))}
-                  </select>
-                </div>
-                <button
-                  className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
-                  onClick={handleCommentSubmit}
+              
+              <div className="mt-6">
+                <button 
+                  onClick={() => router.back()} 
+                  className="text-blue-600 hover:text-blue-800 font-semibold inline-flex items-center"
                 >
-                  Submit Comment
+                  ← Back to Products
                 </button>
+                
               </div>
             </div>
           </div>
+          <div>
+                  <h2 className="text-2xl font-bold text-gray-800 mt-8">Location</h2>
+                <Map/>
+                </div>
+          <div className="mt-8">
+            <h2 className="text-2xl font-bold text-gray-800 mt-8">Comments and Ratings</h2>
+            <div className="mt-4">
+              {comments.map((comment, index) => (
+                <div key={index} className="mb-4 p-4 border rounded-lg shadow-sm">
+                  <p className="text-sm text-gray-600">{comment.date}</p>
+                  <p className="text-lg text-gray-800">{comment.text}</p>
+                  <p className="text-sm text-gray-600">Rating: {comment.rating}/5</p>
+                </div>
+              ))}
+            </div>
+            <div className="mt-4">
+              <textarea
+                className="w-full p-2 border rounded-lg text-black"
+                rows="3"
+                placeholder="Add your comment..."
+                value={newComment}
+                onChange={(e) => setNewComment(e.target.value)}
+              />
+              <div className="mt-2">
+                <label className="text-gray-600">Rating: </label>
+                <select
+                  className="ml-2 p-1 border rounded-lg text-black"
+                  value={rating}
+                  onChange={(e) => setRating(Number(e.target.value))}
+                >
+                  <option value="0">Select Rating</option>
+                  {[1, 2, 3, 4, 5].map(r => (
+                    <option key={r} value={r}>{r}</option>
+                  ))}
+                </select>
+              </div>
+              <button
+                className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+                onClick={handleCommentSubmit}
+              >
+                Submit Comment
+              </button>
+            </div>
+          </div>
+          
         </div>
+       
       </main>
-
+      
       <Footer />
     </>
   );
@@ -184,3 +185,5 @@ function ProductDetailsEx() {
 }
 
 export default ProductDetailsEx;
+
+
